66449dd694b6de15f5938457b9d868ec61734900
Ilija Tomic <ilija.tomic.dev@gmail.com>
2018-03-20 19:51:21 +0100

    Update

---
+++ app/src/main/java/com/ikac/test/screens/home/HomeActivity.java
@@ -0,0 +1,10 @@
+package com.ikac.test.screens.home;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Home screen of application
+ */
+public class HomeActivity extends AppCompatActivity {
+    
+}
